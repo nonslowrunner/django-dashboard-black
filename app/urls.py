318d9225c3d24970688d76ab3887061c6121694b
nonslowrunner <55234398+nonslowrunner@users.noreply.github.com>
2020-07-17 14:15:02 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,19 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from app import views
+from django.conf.urls import url
+
 
 urlpatterns = [
-    # Matches any html file 
-    re_path(r'^.*\.html', views.pages, name='pages'),
-
     # The home page
-    path('', views.index, name='home'),
+    url('^$', views.index, name='home'),
+    url('resume', views.index, name='home'),
+    url('dash', views.dashboard, name='dash'),
+    url('lstm', views.lstm, name='lstm'),
+    url('nlp', views.nlp, name='nlp'),
+    url('covid19', views.covid19, name='covid19'),
+
+
+
 ]
+
